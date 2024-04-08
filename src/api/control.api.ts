f5b5f226fda45f024fa80e6ec408cc756199ed1d
thobui14ig <buithanhtho14ig@gmail.com>
2024-04-08 23:52:16 +0700

    ok

--- src/api/control.api.ts
+++ src/api/control.api.ts
@@ -1,4 +1,4 @@
 import http from './http';
 
 export const getTitleByPath = (path: string) => http.get<IControl>(`/control/path?path=${path}`);
-export const getMenuControls = () => http.get<IControl>(`/role-control`);
+export const getMenuControls = () => http.get<any[]>(`/role-control`);
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,16 +1,7 @@
-import { useEffect } from "react"
 import useAddTab from "../hooks/useAddTab"
-import { getMenuControls } from "../api/control.api"
 
 function Home() {
     useAddTab()
-
-    useEffect(() => {
-        ( async () => {
-            const { data } = await getMenuControls()
-            console.log(123123, data)
-        }) ()
-    }, [])
 
   return (
     <div>
