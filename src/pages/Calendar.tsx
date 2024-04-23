2cdb2f5b4c0d3c1c82b3d558d108af9aa801c2ee
thobui14ig <buithanhtho14ig@gmail.com>
2024-04-23 15:02:36 +0700

    ok

--- src/pages/Calendar.tsx
+++ src/pages/Calendar.tsx
@@ -1,10 +1,6 @@
-import Breadcrumb from '../components/Breadcrumb';
-
 const Calendar = () => {
   return (
     <>
-      <Breadcrumb pageName="Calendar" />
-
       {/* <!-- ====== Calendar Section Start ====== --> */}
       <div className="w-full max-w-full rounded-sm border border-stroke bg-white shadow-default dark:border-strokedark dark:bg-boxdark">
         <table className="w-full">
